361efca77f10c60ecdc274c6310755bc4bf841e6
Samuel Calderon <samuel.calderon@uarm.pe>
2022-12-07 16:53:49 -0500

    add endpoint parameters

--- plumber.R
+++ plumber.R
@@ -10,50 +10,66 @@
 #    https://www.rplumber.io/
 #
 
+list.files("R", full.names = TRUE) |> 
+  purrr::walk(source)
+
 library(plumber)
+library(promises)
+
+future::plan(future::multisession())
 
 #* @apiTitle Reportes API
 
 
-#* Plot a histogram
-#* @serializer png
-#* @get /plot
-function(){
-  rand <- rnorm(100)
-  hist(rand)
-}
-
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-function(a, b){
-  as.numeric(a) + as.numeric(b)
-}
-
 #* Return a pdf
-#* @serializer html
+#* 
+#* @param date_max The maximun date for the report
+#* @param date_min The minimun date for the report
+#* @param group_id The group ID
+#* @param user_id The userID
+#* 
 #* @get /reporte
+#* @serializer html
+function(
+    date_max = as.character(Sys.Date()), 
+    date_min = as.character(Sys.Date() - 15), 
+    group_id = NULL, 
+    user_id = NULL
+  ) {
+  
+  future_promise({
+    tmpdir <- tempdir()
+    R.utils::copyDirectory("inst/templates/reporte_group_user", tmpdir)
+    
+    tmp_input_file <- file.path(tmpdir, "template.rmd")
+    
+    create_reporte_group_user(
+      input = tmp_input_file, 
+      date_max = as.Date(date_max), 
+      date_min = as.Date(date_min),
+      group_id = group_id %||% Sys.getenv("TAREAS_GROUP_ID"),
+      user_id = user_id %||% Sys.getenv("TAREAS_USER_ID")
+    )
+    
+    tmp_outfile <- tools::file_path_sans_ext(tmp_input_file) |> paste0(".html")
+    
+    message("temp_outfile is ", tmp_outfile)
+    
+    readBin(tmp_outfile, "raw", n=file.info(tmp_outfile)$size)
+  })
+  
+}
+
+
+
+
+#* Random number
+#* 
+#* @get /random
 function() {
-  tmpdir <- tempdir()
-  R.utils::copyDirectory("inst/templates/reporte_group_user", tmpdir)
   
-  tmp_input_file <- file.path(tmpdir, "template.rmd")
-  
-  create_reporte_group_user(input = tmp_input_file)
-  
-  tmp_outfile <- tools::file_path_sans_ext(tmp_input_file) |> paste0(".html")
-  
-  message("temp_outfile is ", tmp_outfile)
-
-  readBin(tmp_outfile, "raw", n=file.info(tmp_outfile)$size)
+  future_promise(rnorm(1), seed = NULL)
 }
 
 
-#* Return user data
-#* @param id The user id
-#* @get /user
-function(id) {
-  db$db_get_query("SELECT * FROM users WHERE user_id = {id}", id = id)
-}
 
